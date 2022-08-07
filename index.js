0bbd8fe87795fa31ce6a27bcf20cafe52b2e2cb0
thadeus <thadeusnyariki94@gmail.com>
2022-08-07 13:25:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
